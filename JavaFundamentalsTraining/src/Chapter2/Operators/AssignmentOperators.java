e72dcefcaf6bb8f07647ec14781c869a6e30b7fa
Nokwakhiwa19 <Nokwakhiwa.Mazibuko@ilabqa.com>
2025-09-09 10:53:00 +0200

    Chapter 2 completed. Close #1, Close #3

---
+++ JavaFundamentalsTraining/src/Chapter2/Operators/AssignmentOperators.java
@@ -0,0 +1,25 @@
+package Chapter2.Operators;
+/**
+ * @author Nokwakhiwa Mazibuko
+ * This class shows different assignment operators
+ */
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        //Assign
+        int age =25;
+        //add and assign
+        int a = 5, b=3, c=4, d=12, e=15;
+        a += 3;
+        b*=3;
+        c-=3;
+        d/=3;
+        e%=3;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
+        System.out.println("e=" + e);
+
+    }
+}
---
+++ JavaFundamentalsTraining/src/Chapter2/SelectionStructures/TernaryOperator.java
@@ -0,0 +1,34 @@
+package Chapter2.SelectionStructures;
+
+/**
+ * @author Nokwakhiwa Mazibuko
+ *
+ */
+public class TernaryOperator {
+    public static void main(String[] args) {
+        //        //condition ? resultWhenTrue :resultsWhenFalse
+//        int numOne = 5, numTwo =7;
+//        int smallNumber;
+//
+//        if(numOne < numTwo){
+//            smallNumber = numOne;
+//        }else{
+//            smallNumber = numTwo;
+//        }
+//        //variable = condition ? resultWhenTrue :resultsWhenFalse
+//        smallNumber =numOne < numTwo ? numOne : numTwo;
+
+        int number = 5;
+        String results = "";
+
+//        if(number %2 == 0){
+//           System.out.println("Even");
+//        }else if(number %5 == 0){
+//            System.out.println("Odd");
+//        }
+        results = number %2 == 0 ? "Even" :
+                number %5 == 0 ? "Multiple of 5": "Odd";
+
+        System.out.println(results);
+    }
+}
